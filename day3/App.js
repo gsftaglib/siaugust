fd3f0381ed0ed669599f8c648c17dacca7ec3882
Sithara <sithara.augustine@oracle.com>
2023-01-06 15:44:06 +0530

    Day 3

--- day3/App.js
+++ day3/App.js
@@ -1,36 +1,68 @@
 import React from "react";
 import ReactDOM from "react-dom/client"
-import dice from "./dice.png"
+import icon from "./React-icon.png"
+import user from "./user.png"
 
-const heading = (
-    <h1>This is dcoument Header from JSX</h1>
-);
-const Header = () => {
+/*const heading3 = React.createElement("h3",{
+    className:"title",
+    key:"h3"
+},"Heading 3");
+const heading2 = React.createElement("h2",{
+    className:"title",
+    key:"h2"
+},"Heading 2");
+const heading1 = React.createElement("h1",{
+    className:"title",
+    key:"h1"
+},"Heading 1");
+const divContainer = React.createElement("div",{
+    className:"title"
+},[heading1,heading2,heading3]);*/
+
+const heading1 = <h1>Heading 1</h1>;
+const heading2 = <h2>Heading 2</h2>;
+const heading3 = <h3>Heading 3</h3>;
+
+const Logo = () => {
     return (
-        <div>
-        {heading}
-        <h2>Namaste React from Header Component</h2>
-        <Body />
-        <Footer />        
-        </div>
-    );
-} 
-const Body = () => {
-    return (
-        <div>
-        <h2>This is document Body with a dice image below</h2>
-        <img src={dice} alt="logo"/>
+        <div className="flex-item">
+        <img width='100px' src={icon} />
+        <div>This is logo area</div>
         </div>
     );
 }
-const Footer = () => {
+
+const SearchBar = () => {
     return (
-        <div>
-            <h1>
-                This is document Footer
-            </h1>
+        <div className="flex-item">
+        <div>This is search bar area</div>
         </div>
-    )
+    );
 }
+
+const Usericon = () => {
+    return (
+        <div className="flex-item">
+            <img src={user} width='100px;'/>
+            <div>This is Usericon area</div>
+        </div>
+    );
+}
+
+const Container = () => {
+    return(
+        <div>
+            {heading1}
+            {heading2}
+            {heading3}
+            <div className="flex-container">
+                <Logo />
+                <SearchBar />
+                <Usericon />
+            </div>
+        </div>
+    );
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(<Container />);
